3611e8af183c94d537df5d9053a732ead7cf8d1c
Biswajit Nayak <bnayak.nayak7@gmail.com>
2020-06-05 20:04:02 +0530

    Biswajit: Modified the routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     		get :tweets, to: "tweets#index"
 			post :create_tweet, to: "tweets#create"
 			put 'update_tweet/:id', to: "tweets#update"
-			delete :delete_tweet, to: "tweets#destroy"
+			delete 'delete_tweet/:id', to: "tweets#destroy"
 			post :sign_up, to: 'registrations#create'
 			post :login, to: 'sessions#create'
 			delete :log_out, to: "sessions#logout"
